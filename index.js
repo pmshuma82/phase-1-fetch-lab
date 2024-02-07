dc1733d99b9d49e1c56f7539750d117ee435aef0
Paul Shumate <pmshuma@aol.com>
2024-02-06 21:06:06 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const titlesList = document.createElement("ul");
+  books.forEach((book) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = book.name;
+    titlesList.appendChild(listItem);
   });
+  document.body.appendChild(titlesList);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+fetchBooks()
